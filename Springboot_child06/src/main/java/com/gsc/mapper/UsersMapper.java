1c8edfef4491ceb4f0574d01b84940853d5f23fd
ganshengcai <404603005@qq.com>
2020-04-28 19:41:16 +0800

    添加旅客姓名列表检索

--- Springboot_child06/src/main/java/com/gsc/mapper/UsersMapper.java
+++ Springboot_child06/src/main/java/com/gsc/mapper/UsersMapper.java
@@ -2,7 +2,10 @@ package com.gsc.mapper;
 
 import com.gsc.pojo.Users;
 
+import java.util.List;
+
 public interface UsersMapper {
     int deleteByPrimaryKey(Integer id);
     int delById(int id);
+    List<String> getNameList();
 }
